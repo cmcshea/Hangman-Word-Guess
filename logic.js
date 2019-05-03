1d920f7bd4703d46c57f8204467c9ba3f2658c2a
cmcshea <cory.mcshea@gmail.com>
2019-05-03 06:50:07 -0400

    java updates

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 //creating gamewords array - list of 5 countries around the world
 
-var gamewords = ["colombia", "india", "netherlands", "germany", "hungaray"];
+var gamewords = ["colombia", "india", "netherlands", "germany", "hungary"];
 
 //create a randomWord function - It should take a single argument, an array of words, and should return a random word from that array
 
