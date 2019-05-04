0d9f5e88c04e6cdd84978456c69ef30ab7fdd653
cmcshea <cory.mcshea@gmail.com>
2019-05-03 21:08:59 -0400

    js testing code pushed

--- logic.js
+++ logic.js
@@ -34,3 +34,36 @@ function isCorrectGuess(word, letter ) {
   return false;
 }
 
+// 1.3 - getBlanks function
+// You're going to need some code to generate "blanks" ("_" characters) based on the length of the word.
+// To do this, create a function named getBlanks that takes one parameter: the word. It should return an array of "_" (underscore) characters with length equal to the length of the word.
+// For example:
+// // returns ["_", "_", "_", "_", "_"]
+// getBlanks("hello");
+
+
+
+  
+  function getBlanks(word) {
+    var word = "summer";
+    var blanks = ["_", "_", "_", "_", "_", "_"];
+    return word[blanks];   
+
+}
+
+
+// function randomWord(words) {
+//       var random =  Math.floor(Math.random() * words.length);
+//       return words[random];
+
+// 1.4 - fillBlanks function
+// The final "utility" you'll need is one which will fill a blanks array in the correct locations given a letter and the word that array was built from. 
+// For example:
+// // returns ["h", "_", "_", "_", "_"]
+// fillBlanks("hello", ["_", "_", "_", "_", "_"], "h");
+
+// // returns ["_", "e", "l", "l", "_"]
+// fillBlanks("hello", ["_", "e", "_", "_", "_"], "l");
+// Name your function fillBlanks, and have it take three arguments: the word string, the array of the current puzzle state, and the letter that is going to be filled in (arguments in that order).
+
+function 
